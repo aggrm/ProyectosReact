2329b37a6c0badc64c2140e58c385926c7e84730
Alberto Goujon <alberto.goujon@gmail.com>
2025-11-21 20:22:50 +0100

    calculado el total sin propinas

--- calculadora_propinas/src/componets/OrderTotals.tsx
+++ calculadora_propinas/src/componets/OrderTotals.tsx
@@ -1,10 +1,19 @@
-export default function OrderTotals() {
+import { useMemo } from "react"
+import type { OrderItem } from "../types"
+import { formatCurrency } from "../helpers"
+
+type OrderTotalsProps = {
+  order : OrderItem[]
+}
+export default function OrderTotals({order} : OrderTotalsProps) {
+  
+  const amout = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
   return (
     <>
       <div className="space-y-3">
         <h2 className="font-black text-2xl">Totales y propinas</h2>
         <p>Subtotal a pagar: {''}
-          <span className="font-bold">$0</span>
+          <span className="font-bold">{formatCurrency(amout)}</span>
         </p>
         <p>Propina: {''}
           <span className="font-bold">$0</span>
