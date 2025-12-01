f56397f9c542de4e2ea27b18e25790c6970fdeb2
Alberto Goujon <alberto.goujon@gmail.com>
2025-12-01 04:38:58 +0100

    Añadida la funcionalidad de eliminar registros de actividades

--- calorie-tracker/src/components/ActivityList.tsx
+++ calorie-tracker/src/components/ActivityList.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo, type Dispatch } from 'react'
 import type { Activity } from '../types'
 import { categories } from '../date/categories'
-import { PencilSquareIcon } from '@heroicons/react/24/outline'
+import { PencilSquareIcon, XCircleIcon } from '@heroicons/react/24/outline'
 import type { ActivityActions } from '../reducers/activity-reducer'
 
 type ActivityListProps = {
@@ -15,31 +15,45 @@ export default function ActivityList({activities, dispatch} : ActivityListProps)
         (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : '')
     , [activities])
     
+
+    const isEmptyActivities = useMemo(() => activities.length === 0, [activities])
     return (
         <>
             <h2 className='text-4xl font-bold text-slate-600 text-center'>Comida y Actividades:</h2>
-            {activities.map( activity =>(
-                <div key={activity.id} className='p-5 py-10 bg-white mt-5 flex justify-between'>
-                    <div className='space-y-2 relative'>
-                        <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold 
-                        ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
-                            {categoryName(+activity.category)}
-                        </p>
-                        <p className='text-2xl font-bold pt-5'>{activity.name}</p>
-                        <p className='font-black text-4xl text-lime-500'>{activity.calories} {''}</p>
-                        <span>Calorias</span>
+
+            {isEmptyActivities ? 
+                <p className='text-center my-5'>No hay actividades aún...</p> : 
+                activities.map( activity =>(
+                    <div key={activity.id} className='p-5 py-10 bg-white mt-5 flex justify-between'>
+                        <div className='space-y-2 relative'>
+                            <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold 
+                            ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
+                                {categoryName(+activity.category)}
+                            </p>
+                            <p className='text-2xl font-bold pt-5'>{activity.name}</p>
+                            <p className='font-black text-4xl text-lime-500'>{activity.calories} {''}</p>
+                            <span>Calorias</span>
+                        </div>
+                        <div className='flex gap-5 items-center'>
+                                <button onClick={() => dispatch({
+                                    type: 'set-activeId',
+                                    payload:{id: activity.id}
+                                })}>
+                                    <PencilSquareIcon
+                                        className='h8 w-8 text-gray-800'
+                                    />
+                                </button>
+
+                                <button onClick={() => dispatch({
+                                    type: 'delete-activity',
+                                    payload:{id: activity.id}
+                                })}>
+                                    <XCircleIcon
+                                        className='h8 w-8 text-red-500'
+                                    />
+                                </button>
+                        </div>
                     </div>
-                    <div className='flex gap-5 items-center'>
-                            <button onClick={() => dispatch({
-                                type: 'set-activeId',
-                                payload:{id: activity.id}
-                            })}>
-                                <PencilSquareIcon
-                                    className='h8 w-8 text-gray-800'
-                                />
-                            </button>
-                    </div>
-                </div>
             ))}
         </>
     )
