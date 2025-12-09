041babcb27d8b69b650833f89d133c62156e39d8
Alberto Goujon <alberto.goujon@gmail.com>
2025-12-10 00:55:03 +0100

    Primeros componentes creados

--- pacientes-zustand/src/App.tsx
+++ pacientes-zustand/src/App.tsx
@@ -1,9 +1,21 @@
+import PatientForm from "./componets/PatientForm"
+import PatientList from "./componets/PatientList"
 
 function App() {
 
   return (
     <>
-      <h1 className="text-4xl">holaaaa</h1>
+      <div className="container mx-auto mt-20">
+        <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto">
+          Segimineto de Pacinetes {''}
+          <span className="text-indigo-700">Veterinaria</span>
+        </h1>
+
+        <div className="mt-12 md:flex">
+          <PatientForm />
+          <PatientList />
+        </div>
+      </div>
     </>
   )
 }
