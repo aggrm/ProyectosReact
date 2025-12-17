f5b7d735d218904c477b93c17769c71d48e591dc
Alberto Goujon <alberto.goujon@gmail.com>
2025-12-17 03:36:47 +0100

    añadida api de clima con apii tonken y display de clima

--- clima-react-typescript/src/App.tsx
+++ clima-react-typescript/src/App.tsx
@@ -1,13 +1,20 @@
 import styles from "./App.module.css"
+import Form from "./componets/Form/Form"
+import WeatherDetail from "./componets/Weather/WeatherDetail";
+import useWeather from "./hooks/useWeather"
+
 function App() {
 
+  const {weather, fetchWeather, hasWeatherData} = useWeather();
   return (
     <>
       <h1 className={styles.title}>Buscador de clima</h1>
 
       <div className={styles.container}>
-       <p>1</p>
-       <p>2</p>
+       <Form fetchWeather = {fetchWeather}
+       />
+       {hasWeatherData && <WeatherDetail weather = {weather}/>}
+       
       </div>
     </>
   )
---
+++ clima-react-typescript/src/hooks/useWeather.ts
@@ -0,0 +1,108 @@
+import axios from "axios"
+import type { SearchType } from "../types/indext"
+import z from "zod"
+//import { number, object, string, Output, parse } from "valibot"
+import { useMemo, useState } from "react"
+
+// TYPE GUARD O ASSERTION
+// function isWeatherResponse(weather : unknown) : weather is Weather {
+//     return (
+//         Boolean(weather) &&
+//         typeof weather === 'object' && 
+//         typeof (weather as Weather).name === 'string' &&
+//         typeof (weather as Weather).main.temp === 'number' &&
+//         typeof (weather as Weather).main.temp_max === 'number' &&
+//         typeof (weather as Weather).main.temp_min === 'number' 
+//     )
+// }
+
+// Zod
+const Weather = z.object({
+    name: z.string(),
+    main: z.object({
+        temp: z.number(),
+        temp_max: z.number(),
+        temp_min: z.number(),
+    })
+})
+
+export type Weather = z.infer<typeof Weather>
+
+// Valibot
+// const WeatherSchema = object({
+//     name: string(),
+//     main: object({
+//         temp: number(),
+//         temp_max: number(),
+//         temp_min: number()
+//     })
+// })
+// type Weather = Output<typeof WeatherSchema>
+
+const initialState = {
+    name: '',
+    main: {
+        temp: 0,
+        temp_max: 0,
+        temp_min: 0
+    }
+}
+
+
+
+export default function useWeather() {
+
+    const [weather, setWeather] = useState<Weather>(initialState)
+
+    const fetchWeather = async (search: SearchType) => {
+        const appId = import.meta.env.VITE_API_KEY
+        try {
+            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+            const {data: geoResult} = await axios(geoUrl)
+            
+            const lat = geoResult[0].lat
+            const lon = geoResult[0].lon
+
+            const wetherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+            
+            // Castear el type
+            // const {data: weatherResult} = await axios<Weather>(weatherUrl)
+            // console.log(weatherResult.temp)
+            // console.log(weatherResult.name)
+
+            // Type Guards
+            // const {data: weatherResult} = await axios(weatherUrl)
+            // const result = isWeatherResponse(weatherResult)
+            // if(result) {
+            //     console.log(weatherResult.name)
+            // } else {
+            //     console.log('Respuesta mal formada')
+            // }
+
+            //Zod
+            const {data: wetherResult} = await axios(wetherUrl)
+            const result = Weather.safeParse(wetherResult)
+            if(result.success) {
+                setWeather(result.data)
+            }
+
+            // Valibot
+            // const {data: weatherResult} = await axios(weatherUrl)
+            // const result = parse(WeatherSchema, weatherResult)
+            // if(result) {
+            //     console.log(result.name)
+            // }
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const hasWeatherData = useMemo(() => weather.name , [weather])
+
+    return{
+        weather,
+        hasWeatherData,
+        fetchWeather
+    }
+}
