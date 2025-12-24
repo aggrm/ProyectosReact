2654ed490e3cd2c62e92dd8d7dddc266d63a4246
Alberto Goujon <alberto.goujon@gmail.com>
2025-12-24 20:04:36 +0100

    Estructura de paginas montado a traves de react router

---
+++ bebidas-react-typescript/src/router.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import IndexPage from "./views/IndexPage"
+import FavoritesPage from "./views/FavoritesPage"
+import Layouts from "./layouts/Layouts"
+
+export default function AppRouter() {
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Layouts/>}>
+                    <Route path="/" element={<IndexPage/>} index />
+                    <Route path="/favoritos" element={<FavoritesPage/>} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
