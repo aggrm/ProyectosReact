38013400eab89dbe8fc945f744709ecc272a9124
Alberto Goujon <alberto.goujon@gmail.com>
2026-01-06 05:48:10 +0100

    Frontal comunicado con back y api  gracias a react router y axios

--- fullstack_node_react_ts/client/src/router.tsx
+++ fullstack_node_react_ts/client/src/router.tsx
@@ -1,7 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layouts/Layout";
-import Products from "./views/Products";
-import NewProduct, { action as newProductAction } from "./layouts/NewProduct";
+import Products, { action as updateAvailabilityAction, loader as produtsLoader} from "./views/Products";
+import NewProduct, { action as newProductAction } from "./views/NewProduct";
+import EditProduct, { loader as editProductLoader, action as editProductAction} from "./views/EditProduct";
+import { action as deleteProdutAction } from "./components/ProductDetails";
+
 
 export const router = createBrowserRouter([
     {
@@ -10,13 +13,25 @@ export const router = createBrowserRouter([
         children: [
             {
                 index: true,
-                element:<Products />
+                element:<Products />,
+                loader: produtsLoader,
+                action: updateAvailabilityAction
             },
             {
                 path: 'productos/nuevo',
                 element: <NewProduct />,
                 action: newProductAction
-            }
+            },
+            {
+                path: 'productos/:id/editar', //ROA Pattern - Resource-oriented desing
+                element: <EditProduct />,
+                loader: editProductLoader,
+                action: editProductAction
+            },
+            {
+                path: 'productos/:id/eliminar', //ROA Pattern - Resource-oriented desing
+                action: deleteProdutAction
+            },
         ]
     }
 ])
--- fullstack_node_react_ts/server/src/server.ts
+++ fullstack_node_react_ts/server/src/server.ts
@@ -1,9 +1,11 @@
-import express from "express";
-import router from "./router";
-import db from "./config/db";
+import express from "express"
 import colors from "colors"
+import cors, { CorsOptions } from "cors";
+import morgan from 'morgan'
 import swaggerUI from "swagger-ui-express"
 import swaggerSepc, {swaggerUIOptions} from "./config/swagger";
+import router from "./router"
+import db from "./config/db"
 
 //conectar a base de datos
 export async function connectDB() {
@@ -21,8 +23,22 @@ connectDB()
 // Instancia de express
 const server = express()
 
+//Permitir conexiones
+const corsOptions : CorsOptions = {
+    origin: function(origen, callback) {
+        if(origen === process.env.FRONTEND_URL){
+            callback(null, true)
+        }else{
+            callback(new Error('Error de CORS'), false)
+        }
+    }
+}
+server.use(cors(corsOptions))
+
 // Leer datos de formularios
 server.use(express.json())
+
+server.use(morgan('dev'))
 server.use('/api/products', router)
 
 //Docs
